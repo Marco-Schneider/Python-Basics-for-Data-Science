65d9cd8443f534aee213c10b97a3a9a606874846
Marco-Schneider <58363718+Marco-Schneider@users.noreply.github.com>
2020-07-28 15:50:17 -0300

    Quizzes about dictionaries

---
+++ Exercises/Dictionaries.py
@@ -0,0 +1,14 @@
+#Question 1
+#Consider the following dictionary
+
+D={'a':0,'b':1,'c':2}
+
+#What is the result of the following: D.values()
+
+dict_values = D.values()
+print(dict_values)
+
+#Question 2
+#What is the output of the following: D['b']
+
+print(D['b'])
