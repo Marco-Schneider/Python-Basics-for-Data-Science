9a910733bcdca4d65ed68bda0040249408cfbf79
Marco-Schneider <58363718+Marco-Schneider@users.noreply.github.com>
2020-07-28 15:10:54 -0300

    Exercises about sets

---
+++ Exercises/Sets.py
@@ -0,0 +1,16 @@
+#Question 1
+#What is the result of the following lines of code?
+
+S = {"A", "B", "C"}
+U = {"A", "Z", "C"}
+
+C = U.union(S)
+
+print(C)
+
+#Question 2
+#What is the intersection of set S and U?
+
+D = S.intersection(U)
+
+print(D)
