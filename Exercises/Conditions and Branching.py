db17b50819cbb7f82401a6a48e0e48e57432a132
Marco-Schneider <58363718+Marco-Schneider@users.noreply.github.com>
2020-07-29 15:46:29 -0300

    Conditions and Branching exercises

---
+++ Exercises/Conditions and Branching.py
@@ -0,0 +1,15 @@
+#Question 1
+#Find the values of i that produces a True for the following
+
+#i!=0, all the real numbers, except zero
+
+#Question 2
+#What is the output of the following
+
+x = "a"
+
+if(x!="a"):
+    print("this is not a")
+else:
+    print("this is a")
+    
